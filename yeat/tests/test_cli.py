cee0b716aed2fabacf0d82df752102b86d83cf43
Dane Jo <danejo3@outlook.com>
2021-11-11 13:03:05 -0500

    updating test_cli.py

--- yeat/tests/test_cli.py
+++ yeat/tests/test_cli.py
@@ -5,6 +5,7 @@ from yeat import cli
 from yeat.tests import data_file
 
 
+# the purpose of this test is to check if the snakemake workflow is properly defined.
 def test_basic_dry_run(tmp_path):
     wd = str(tmp_path)
     os.makedirs(wd, exist_ok=True)
@@ -19,7 +20,7 @@ def test_basic_dry_run(tmp_path):
         "Animal_289",
         "-n",
     ]
-    parser = yeat.cli.get_parser()  # .parse_args(arglist)
+    parser = yeat.cli.get_parser()
     yeat.cli.add_args(parser)
     args = parser.parse_args(arglist)
     yeat.cli.main(args)

5c117aa43921dde7c5f6a45b647068204dffa547
Dane <danejo3@outlook.com>
2023-06-27 15:13:30 -0400

    first commit

--- yeat/cli/config.py
+++ yeat/cli/config.py
@@ -13,8 +13,14 @@ from warnings import warn
 
 
 PAIRED = ("spades", "megahit", "unicycler")
-PACBIO = ("canu", "flye")
+PACBIO = ("canu", "flye", "hifiasm", "hifiasm-meta")
+OXFORD = ("canu", "flye")
 ALGORITHMS = PAIRED + PACBIO
+
+SHORT_READS = ("paired", "single", "interleaved")
+PACBIO_READS = ("pacbio-raw", "pacbio-corr", "pacbio-hifi")
+OXFORD_READS = ("nano-raw", "nano-corr", "nano-hq")
+READ_TYPES = SHORT_READS + PACBIO_READS + OXFORD_READS 
 
 
 class AssemblyConfigurationError(ValueError):
@@ -24,7 +30,7 @@ class AssemblyConfigurationError(ValueError):
 class AssemblerConfig:
     def __init__(self, data, threads):
         self.data = data
-        self.validate()
+        # self.validate()
         self.threads = threads
         self.create_sample_and_assembler_objects()
         self.batch()
@@ -60,26 +66,37 @@ class AssemblerConfig:
         for key, value in self.data["samples"].items():
             self.samples[key] = Sample(value)
         self.assemblers = [
-            Assembler.from_data(assembler, self.threads) for assembler in self.data["assemblers"]
+            Assembler(assembler, self.threads) for assembler in self.data["assemblers"]
         ]
 
+    def determine_assembler_workflow(self, assembler):
+        for sample in assembler.samples:
+            if self.samples[sample].type in ("short"):
+                self.paired_assemblers.append(assembler)
+            elif self.samples[sample].type in ("pacbio-raw", "pacbio-corr", "pacbio-hifi"):
+                self.pacbio_assemblers.append(assembler)
+            elif self.samples[sample].type in ("nano-raw", "nano-corr", "nano-hq"):
+                self.oxford_assemblers.append(assembler)
+
     def batch(self):
-        paired_assemblers = []
-        pacbio_assemblers = []
+        self.paired_assemblers = []
+        self.pacbio_assemblers = []
+        self.oxford_assemblers = []
         for assembler in self.assemblers:
-            if assembler.algorithm in PAIRED:
-                paired_assemblers.append(assembler)
-            elif assembler.algorithm in PACBIO:
-                pacbio_assemblers.append(assembler)
+            self.determine_assembler_workflow(assembler)
         self.batch = {
             "paired": {
-                "samples": self.get_batch_samples(paired_assemblers),
-                "assemblers": paired_assemblers,
+                "samples": self.get_batch_samples(self.paired_assemblers),
+                "assemblers": self.paired_assemblers,
             },
             "pacbio": {
-                "samples": self.get_batch_samples(pacbio_assemblers),
-                "assemblers": pacbio_assemblers,
+                "samples": self.get_batch_samples(self.pacbio_assemblers),
+                "assemblers": self.pacbio_assemblers,
             },
+            "oxford": {
+                "samples": self.get_batch_samples(self.oxford_assemblers),
+                "assemblers": self.oxford_assemblers,
+            }
         }
 
     def get_batch_samples(self, assemblers):
@@ -113,33 +130,32 @@ class AssemblerConfig:
 
 class Sample:
     def __init__(self, data):
-        sample = []
-        for fastq in data:
-            read = Path(fastq).resolve()
-            if not read.is_file():
-                raise FileNotFoundError(f"No such file: '{read}'")
-            if str(read) in sample:
-                message = f"Found duplicate read sample: '{read}'"
-                raise AssemblyConfigurationError(message)
-            sample.append(str(read))
-        self.sample = sample
+        for key, value in data.items():
+            #check the key and make sure that type is in READ_TYPES
+            self.type = key
+            sample = []
+            for fastq in value:
+                read = Path(fastq).resolve()
+                if not read.is_file():
+                    raise FileNotFoundError(f"No such file: '{read}'")
+                if str(read) in sample:
+                    message = f"Found duplicate read sample: '{read}'"
+                    raise AssemblyConfigurationError(message)
+                sample.append(str(read))
+            self.sample = sample
 
 
 class Assembler:
-    def __init__(self, label, algorithm, samples, extra_args, threads):
-        self.label = label
-        if algorithm not in ALGORITHMS:
-            raise AssemblyConfigurationError(f"Unsupported assembly algorithm '{algorithm}'")
-        self.algorithm = algorithm
-        self.samples = samples
-        self.extra_args = extra_args
+    def __init__(self, data, threads):
+        self.label = data["label"]
+        if data["algorithm"] not in ALGORITHMS:
+            raise AssemblyConfigurationError(f"Unsupported assembly algorithm '{data['algorithm`']}'")
+        self.algorithm = data["algorithm"]
+        self.samples = data["samples"]
+        self.extra_args = data["extra_args"]
         self.threads = threads
-        if algorithm == "canu":
+        if data["algorithm"] == "canu":
             self.check_canu_required_params()
-
-    @classmethod
-    def from_data(cls, data, threads):
-        return cls(data["label"], data["algorithm"], data["samples"], data["extra_args"], threads)
 
     def check_canu_required_params(self):
         if "genomeSize=" not in self.extra_args:

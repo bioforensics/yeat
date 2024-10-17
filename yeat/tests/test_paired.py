aee6504c6e4c3c5d3988e1fbf9e93079080fdae0
Dane <danejo3@outlook.com>
2024-10-17 14:17:50 -0400

    saving work

--- yeat/tests/test_paired.py
+++ yeat/tests/test_paired.py
@@ -25,7 +25,8 @@ def test_paired_end_assemblers_dry_run(tmp_path):
 
 @pytest.mark.long
 @pytest.mark.illumina
-@pytest.mark.parametrize("algorithm", ["spades", "megahit", "unicycler", "velvet"])
+# @pytest.mark.parametrize("algorithm", ["spades", "megahit", "unicycler", "velvet"])
+@pytest.mark.parametrize("algorithm", ["velvet"])
 def test_paired_end_assemblers(algorithm, capsys, tmp_path):
     wd = str(tmp_path)
     config = write_config(algorithm, wd, "paired.cfg")

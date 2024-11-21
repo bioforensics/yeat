3c38d414cf53227f55b1c9f823c7c59442f60e36
danejo3 <33472323+danejo3@users.noreply.github.com>
2024-11-21 14:22:59 -0500

    New short-read assembler: PenguiN (#82)

--- yeat/config/assembly.py
+++ yeat/config/assembly.py
@@ -12,8 +12,8 @@ from sys import platform
 
 
 ALGORITHMS = {
-    "paired": ("spades", "megahit", "unicycler"),
-    "single": ("spades", "megahit", "unicycler"),
+    "paired": ("spades", "megahit", "unicycler", "penguin"),
+    "single": ("spades", "megahit", "unicycler", "penguin"),
     "pacbio": ("canu", "flye", "hifiasm", "hifiasm_meta", "unicycler", "metamdbg"),
     "oxford": ("canu", "flye", "unicycler"),
     "hybrid": ("unicycler"),
@@ -93,7 +93,7 @@ class Assembly:
     def get_qa_file(self, sample):
         readtype = self.get_readtype(sample)
         algorithm_dir = f"analysis/{sample.label}/{readtype}/{self.label}/{self.algorithm}"
-        if self.bandage:
+        if self.bandage and self.algorithm != "penguin":
             return f"{algorithm_dir}/bandage/.done"
         return f"{algorithm_dir}/quast/report.html"
 

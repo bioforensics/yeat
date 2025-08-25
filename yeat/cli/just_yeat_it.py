697dff096fd7737fa4724a22eb995682838a9d53
danejo3 <33472323+danejo3@users.noreply.github.com>
2025-08-25 15:51:46 -0400

    Restructuring the output directory (#91)

--- yeat/cli/just_yeat_it.py
+++ yeat/cli/just_yeat_it.py
@@ -7,144 +7,102 @@
 # # Development Center.
 # # -------------------------------------------------------------------------------------------------
 
-from argparse import ArgumentParser
-from pathlib import Path
-import toml
-from yeat.workflow import run_workflow
-from yeat.cli.cli import workflow_configuration
-from yeat import __version__
+# from . import cli, illumina
+# from argparse import ArgumentParser
+# import json
+# from pathlib import Path
+# from yeat import workflow
 
 
-def main(args=None):
-    if args is None:
-        args = get_parser().parse_args()
-    create_config(args)
-    add_config(args)
-    run_workflow(args)
+# def main(args=None):
+#     if args is None:
+#         args = get_parser().parse_args()  # pragma: no cover
+#     create_config(args)
+#     add_config(args)
+#     if not args.grid:
+#         setattr(args, "grid", False)
+#     workflow.run_workflow(args)
 
 
-def get_parser(exit_on_error=True):
-    parser = ArgumentParser(exit_on_error=exit_on_error)
-    positional_args(parser)
-    parser.add_argument(
-        "-v",
-        "--version",
-        action="version",
-        version=f"YEAT v{__version__}",
-    )
-    workflow_configuration(parser)
-    fastp_configuration(parser)
-    downsample_configuration(parser)
-    sample_configuration(parser)
-    algorithm_configuration(parser)
-    return parser
+# def get_parser(exit_on_error=True):
+#     parser = ArgumentParser(exit_on_error=exit_on_error)
+#     parser._optionals.title = "options"
+#     cli.workflow_configuration(parser)
+#     cli.grid_configuration(parser)
+#     illumina.fastp_configuration(parser)
+#     illumina.downsample_configuration(parser, just_yeat_it=True)
+#     sample_configuration(parser)
+#     algorithm_configuration(parser)
+#     positional_args(parser)
+#     return parser
 
 
-def positional_args(parser):
-    parser._positionals.title = "required arguments"
-    parser.add_argument("reads", type=str, help="paired-end reads in FASTQ format")
+# def sample_configuration(parser):
+#     sample = parser.add_argument_group("sample configuration")
+#     sample.add_argument(
+#         "--sample-label",
+#         default="sample1",
+#         help='set the sample label; by default, "sample1"',
+#         metavar="STR",
+#     )
 
 
-def fastp_configuration(parser):
-    illumina = parser.add_argument_group("fastp configuration")
-    illumina.add_argument(
-        "-l",
-        "--length-required",
-        default=50,
-        help="discard reads shorter than the required L length after pre-preocessing; by default, L=50",
-        metavar="L",
-        type=int,
-    )
+# def algorithm_configuration(parser):
+#     algorithm = parser.add_argument_group("algorithm configuration")
+#     algorithm.add_argument(
+#         "--assembly-label",
+#         default="assembly1",
+#         help='set the assembly label; by default, "assembly1"',
+#         metavar="STR",
+#     )
+#     algorithm.add_argument(
+#         "--algorithm",
+#         default="spades",
+#         help='substitute the default assembly algorithm with another algorithm; for example, "megahit" or "unicycler"; by default, "spades"',
+#         metavar="STR",
+#     )
+#     algorithm.add_argument(
+#         "--extra-args",
+#         default="",
+#         help='add assembly algorithm flags; for example, "--meta" or "--isolate --careful" for SPAdes; by default, empty string',
+#         metavar="STR",
+#     )
 
 
-def downsample_configuration(parser):
-    illumina = parser.add_argument_group("downsampling configuration")
-    illumina.add_argument(
-        "-d",
-        "--downsample",
-        default=0,
-        help="randomly sample D reads from the input rather than assembling the full set; set D=0 to perform auto-downsampling to a desired level of coverage (see --coverage-depth); set D=-1 to disable downsampling; by default, D=0",
-        metavar="D",
-        type=int,
-    )
-    illumina.add_argument(
-        "-g",
-        "--genome-size",
-        default=0,
-        help="provide known genome size in base pairs (bp); by default, G=0",
-        metavar="G",
-        type=int,
-    )
-    illumina.add_argument(
-        "-c",
-        "--coverage-depth",
-        default=150,
-        help="target an average depth of coverage Cx when auto-downsampling; by default, C=150",
-        metavar="C",
-        # type=check_positive,
-    )
+# def positional_args(parser):
+#     parser._positionals.title = "required arguments"
+#     parser.add_argument("reads", type=str, nargs=2, help="paired-end reads in FASTQ format")
 
 
-def sample_configuration(parser):
-    sample = parser.add_argument_group("sample configuration")
-    sample.add_argument(
-        "--sample-label",
-        default="sample1",
-        help='set the sample label; by default, "sample1"',
-        metavar="STR",
-    )
+# def create_config(args):
+#     data = get_config_data(args)
+#     outdir = Path(args.outdir)
+#     outdir.mkdir(parents=True, exist_ok=True)
+#     outfile = open(outdir / "config.cfg", "w")
+#     json.dump(data, outfile, indent=4)
 
 
-def algorithm_configuration(parser):
-    algorithm = parser.add_argument_group("algorithm configuration")
-    algorithm.add_argument(
-        "--assembly-label",
-        default="assembly1",
-        help='set the assembly label; by default, "assembly1"',
-        metavar="STR",
-    )
-    algorithm.add_argument(
-        "--algorithm",
-        default="spades",
-        help='substitute the default assembly algorithm with another algorithm; for example, "megahit" or "unicycler"; by default, "spades"',
-        metavar="STR",
-    )
-    algorithm.add_argument(
-        "--arguments",
-        default="",
-        help='add assembly algorithm flags; for example, "--meta" or "--isolate --careful" for SPAdes; by default, empty string',
-        metavar="STR",
-    )
+# def get_config_data(args):
+#     return {
+#         "samples": {
+#             args.sample_label: {
+#                 "paired": [args.reads],
+#                 "downsample": args.downsample,
+#                 "genome_size": args.genome_size,
+#                 "coverage_depth": args.coverage_depth,
+#             },
+#         },
+#         "assemblies": {
+#             args.assembly_label: {
+#                 "algorithm": args.algorithm,
+#                 "extra_args": args.extra_args,
+#                 "samples": [args.sample_label],
+#                 "mode": "paired",
+#             }
+#         },
+#     }
 
 
-def create_config(args):
-    data = get_config_data(args)
-    workdir = Path(args.workdir)
-    workdir.mkdir(parents=True, exist_ok=True)
-    outfile = open(workdir / "config.toml", "w")
-    toml.dump(data, outfile)
-
-
-def get_config_data(args):
-    return {
-        "samples": {
-            args.sample_label: {
-                "illumina": args.reads,
-                "downsample": args.downsample,
-                "genome_size": args.genome_size,
-                "coverage_depth": args.coverage_depth,
-            },
-        },
-        "assemblers": {
-            args.assembly_label: {
-                "algorithm": args.algorithm,
-                "arguments": args.arguments,
-                "samples": [args.sample_label],
-            }
-        },
-    }
-
-
-def add_config(args):
-    config = Path(args.workdir) / "config.toml"
-    setattr(args, "config", str(config.resolve()))
+# def add_config(args):
+#     config = Path(args.outdir) / "config.cfg"
+#     setattr(args, "config", str(config.resolve()))

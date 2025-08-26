702655be3a1b6b72a73df7e905bb2b8d5616abfa
Dane <danejo3@outlook.com>
2025-08-26 09:20:51 -0400

    rebasing

--- yeat/cli/just_yeat_it.py
+++ yeat/cli/just_yeat_it.py
@@ -1,11 +1,11 @@
-# -------------------------------------------------------------------------------------------------
-# Copyright (c) 2024, DHS. This file is part of YEAT: http://github.com/bioforensics/yeat
-#
-# This software was prepared for the Department of Homeland Security (DHS) by the Battelle National
-# Biodefense Institute, LLC (BNBI) as part of contract HSHQDC-15-C-00064 to manage and operate the
-# National Biodefense Analysis and Countermeasures Center (NBACC), a Federally Funded Research and
-# Development Center.
-# -------------------------------------------------------------------------------------------------
+# # -------------------------------------------------------------------------------------------------
+# # Copyright (c) 2024, DHS. This file is part of YEAT: http://github.com/bioforensics/yeat
+# #
+# # This software was prepared for the Department of Homeland Security (DHS) by the Battelle National
+# # Biodefense Institute, LLC (BNBI) as part of contract HSHQDC-15-C-00064 to manage and operate the
+# # National Biodefense Analysis and Countermeasures Center (NBACC), a Federally Funded Research and
+# # Development Center.
+# # -------------------------------------------------------------------------------------------------
 
 from argparse import ArgumentParser
 from pathlib import Path
